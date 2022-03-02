a222e025d108e3b86a9e8bf507bf6f2afc4736b8
Sunist <sunist@mail.swu-acm.cn>
2022-03-02 14:52:35 +0800

    ⚒️| update main-window

---
+++ Libs/SliceProperty.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MicroscopeDataManager.Libs
+{
+    public class SliceProperty
+    {
+        public string Name { get; set; } = "name";
+        public string Author { get; set; } = "author";
+        public string Description { get; set; } = "description";
+        public string Version { get; set; } = "version";
+        public string FilePath { get; set; } = "filepath";
+        public string Thumbnail { get; set; } = "thumbnail";
+
+        public bool Pack(string outputPath)
+        {
+            try
+            {
+                // pack xml file
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(SliceProperty));
+                    xmlSerializer.Serialize(writer, this);
+                }
+
+                // pack image
+                
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            return true;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MicroscopeDataManager.Libs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,6 @@ namespace MicroscopeDataManager
         {
             InitializeComponent();
         }
+
     }
 }
