0849d9fa863a9d701bde3793303e2305f98476a5
Sunist <sunist@mail.swu-acm.cn>
2022-03-02 17:45:28 +0800

    ⚒️| update main-window ui and slice previewer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,5 +26,156 @@ namespace MicroscopeDataManager
             InitializeComponent();
         }
 
+        private void Menu_File_Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_New_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Export_Package_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Export_Xml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Export_Slice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Import_Xml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Import_Slice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Undo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Paste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_License_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_Prefrence_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Setting_Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_Light_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_HalfDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_HalfUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_PreviewOn_Click(object sender, RoutedEventArgs e)
+        {
+            Slice.Visibility = Visibility.Visible;
+        }
+
+        private void Toolbox_PreviewOff_Click(object sender, RoutedEventArgs e)
+        {
+            Slice.Visibility = Visibility.Hidden;
+        }
+
+        private void Toolbox_ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Slice_Magnifier.Scale < 10)
+            {
+                Slice_Magnifier.Scale = 10;
+            } else if (Slice_Magnifier.Scale < 40)
+            {
+                Slice_Magnifier.Scale = 40;
+            }
+        }
+
+        private void Toolbox_ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            if (Slice_Magnifier.Scale > 10)
+            {
+                Slice_Magnifier.Scale = 10;
+            } else if (Slice_Magnifier.Scale > 4)
+            {
+                Slice_Magnifier.Scale = 4;
+            }
+        }
+
+        private void Toolbox_SmallDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_LargeDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_LargeUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Toolbox_SmallUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
